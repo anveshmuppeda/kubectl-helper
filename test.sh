948bc7ead907f8432d4515d16cc78a3f15d0b685
Anvesh Muppeda <muppedaanvesh@gmail.com>
2024-01-20 17:23:53 -0600

    test

--- test.sh
+++ test.sh
@@ -14,9 +14,17 @@ name_width=10
 age_width=4
 city_width=15
 
+# Function to print a centered string with padding
+centered() {
+  local width=$1
+  local text=$2
+  local padding=$((($width - ${#text}) / 2))
+  printf "%*s%s%*s" $padding "" "$text" $padding ""
+}
+
 # Print table header with centered headers
-printf "+%-*s+%-*s+%-*s+\n" "$name_width" "Name" "$age_width" "Age" "$city_width" "City"
-printf "|%*s+%*s+%*s+\n" "$name_width" "" "$age_width" "" "$city_width" ""
-printf "|%*s|%*s|%*s|\n" "$name_width" "$name1" "$age_width" "$age1" "$city_width" "$city1"
-printf "|%*s|%*s|%*s|\n" "$name_width" "$name2" "$age_width" "$age2" "$city_width" "$city2"
-printf "+%-*s+%-*s+%-*s+\n" "$name_width" "" "$age_width" "" "$city_width" ""
+printf "+%s+%s+%s+\n" "$(centered $name_width 'Name')" "$(centered $age_width 'Age')" "$(centered $city_width 'City')"
+printf "|%s|%s|%s|\n" "$(centered $name_width '')" "$(centered $age_width '')" "$(centered $city_width '')"
+printf "|%s|%s|%s|\n" "$(centered $name_width "$name1")" "$(centered $age_width "$age1")" "$(centered $city_width "$city1")"
+printf "|%s|%s|%s|\n" "$(centered $name_width "$name2")" "$(centered $age_width "$age2")" "$(centered $city_width "$city2")"
+printf "+%s+%s+%s+\n" "$(centered $name_width '')" "$(centered $age_width '')" "$(centered $city_width '')"
